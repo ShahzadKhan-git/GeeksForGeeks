bf3d78beed59abbd9d1847fbbe9c28e92931c290
Shahzad Khan <shahzad14103@gmail.com>
2025-11-12 21:45:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+       int CHAR = 256;
+       int[] count = new int[CHAR];
+       
+       for(int i=0; i<s.length(); i++){
+           count[s.charAt(i)]++;
+       }
+       
+       for(int i=0; i<s.length(); i++){
+           if(count[s.charAt(i)]==1){
+               return s.charAt(i);
+           }
+       }
+       
+       return '$';
+        
+    }
+}
